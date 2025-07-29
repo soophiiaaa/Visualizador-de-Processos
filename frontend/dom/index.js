022a44c77166df326551f6a53b0ef209e55d7d6e
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-07-29 12:46:13 -0300

    feat: add close-button in register

--- frontend/dom/index.js
+++ frontend/dom/index.js
@@ -1,4 +1,5 @@
 const buttonOpen = document.getElementsByClassName("button-open")[0]
+const buttonAdd = document.getElementsByClassName("button-add")[0]
 const buttonClose = document.getElementsByClassName("button-close")[0]
 const modal = document.getElementsByClassName("modal")[0]
 
