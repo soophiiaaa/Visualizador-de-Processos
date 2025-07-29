9a63a3d43bdf18ef4aa0050f1e5653234862abc4
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-07-28 21:34:19 -0300

    feat: start dialog

---
+++ frontend/dom/index.js
@@ -0,0 +1,11 @@
+const buttonOpen = document.getElementsByClassName("button-open")[0]
+const buttonClose = document.getElementsByClassName("button-close")[0]
+const modal = document.getElementsByClassName("modal")[0]
+
+buttonOpen.onclick = function () {
+    modal.showModal()
+}
+
+buttonClose.onclick = function () {
+    modal.close()
+}
